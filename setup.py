b822ab8f021083b81bfa58ede1d7c2929757ae4e
Artur Shaik <artur@shaik.link>
2023-11-03 11:54:51 +0600

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='CueParser',
-    version='1.3.0',
+    version='1.3.1',
 
     description='Simple cue file parser.',
     long_description=long_description,
